d038f4f31cfc64ec461b57b8236123135f13e7ed
Nguyễn Trần Nhật Đức <68223190+nhatduc-nguyentran@users.noreply.github.com>
2021-04-14 14:44:33 +0700

    update runTer

--- Final Project/runTer.py
+++ Final Project/runTer.py
@@ -46,6 +46,45 @@ def writeTagFile(file_path):
 
     workbook.close()
 
+#Replace file
+def replaceFile(file_replace_path, file_data_path, new_data_path):
+    dataReplace = pd.read_excel(file_replace_path, engine = 'openpyxl')
+    dataReplaceFrame = pd.DataFrame(dataReplace, columns=['word','word_replace'])
+
+    data = pd.read_excel(file_data_path, engine='openpyxl')
+    dataFrame = pd.DataFrame(data, columns=['Emotion','Sentence'])
+    
+    workbook = xlsxwriter.Workbook(new_data_path)
+    worksheet = workbook.add_worksheet()
+
+    worksheet.write(0, 1, "Emotion")
+    worksheet.write(0, 2, "Sentence")
+
+    for index in range(len(dataFrame)):
+        tag = dataFrame.Emotion[index]
+        beautiData = []
+        for indexReplace in range(len(dataReplaceFrame)):
+            beautiData.append(dataFrame.Sentence[index].replace(dataReplaceFrame.word[indexReplace], dataReplaceFrame.word_replace[indexReplace]))
+        worksheet.write(index+1, 0, index)
+        worksheet.write(index+1, 1, tag)    
+        worksheet.write(index+1, 2 , beautiData[len(dataReplaceFrame)-1])
+    workbook.close()
+    print("DONE!")
+        
+    return 
+
+def menu():
+    print("1: Add tag\n")
+    print("2: Smooth data\n")
+    option = int(input("Option: "))
+    if(option==1):
+        nameFile = input("Enter name file: ")
+        writeTagFile(nameFile)
+    else:
+        file_replace = input("Enter file replace path: ")
+        file_data = input("Enter file data path: ")
+        file_output = input("Enter file output path: ")
+        replaceFile(file_replace, file_data, file_output)
+
 if __name__ == "__main__":
-    nameFile = input("Enter name file: ")
-    writeTagFile(nameFile)
+    menu()
